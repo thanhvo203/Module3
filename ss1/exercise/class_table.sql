19b93716e1efab3c97a407cef7d0de34a5ab855b
thanhvo203 <vohaithanh21@gmail.com>
2023-05-12 11:25:34 +0700

    bai tap ss1

--- ss1/exercise/class_table.sql
+++ ss1/exercise/class_table.sql
@@ -10,5 +10,12 @@ create table teacher(
 insert into teacher (nameTeacher, age, country)
 values ("Tài","18","Huế"), ('Thành','23','Đà Nẵng');
 
+create table class(
+id int primary key auto_increment,
+`name` varchar (55) not null
+);
+insert into class(`name`)
+values('Tài'),('Thành');
+
 select * from teacher;
 select * from class;
