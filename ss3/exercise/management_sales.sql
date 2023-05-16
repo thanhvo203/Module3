4c7909489690c53cb1d1c7acd5111653715e9103
thanhvo203 <vohaithanh21@gmail.com>
2023-05-16 16:03:12 +0700

    ss3 16/5

---
+++ ss3/exercise/management_sales.sql
@@ -0,0 +1,85 @@
+create database management_sales;
+use management_sales;
+
+create table customer(
+customer_id int primary key auto_increment,
+customer_name varchar(50) not null,
+customer_age tinyint
+);
+create table `order` (
+order_id int primary key auto_increment,
+customer_id int,
+foreign key(customer_id) references customer(customer_id),
+order_date datetime,
+order_total_price int
+);
+create table product(
+product_id int primary key auto_increment,
+product_name varchar(50) not null,
+product_price int
+);
+create table order_detail(
+order_id int,
+product_id int,
+order_quality int,
+primary key(order_id , product_id),
+foreign key(order_id) references `order`(order_id),
+foreign key(product_id) references product(product_id)
+);
+
+insert into customer (customer_name , customer_age)
+values ('Minh Quan', 10),
+       ('Ngoc Oanh' , 20),
+       ('Hong Ha' , 50);
+insert into `order` ( customer_id , order_date, order_total_price)
+values (1,'2006-3-21 15:45:21',null),
+ (2,'2006-3-23 15:45:21',null),
+ (1,'2006-3-16 14:56:59',null);
+
+
+ insert into product ( product_name , product_price )
+ values ( 'May Giat' , 3 ),
+  ( 'Tu Lanh' , 5 ),
+  ( 'Dieu Hoa' , 7 ),
+  ( 'Quat' , 2),
+  ( 'Bep Dien ', 1 );
+  
+insert into order_detail ( order_id , product_id , order_quality )
+values ( 1,1,3),
+       (1,3,7),
+       (1,4,2),
+       (2,1,1),
+       (3,1,8),
+       (2,5,4),
+       (2,3,3);
+
+-- Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
+select order_id , order_date, order_total_price 
+from `order`;
+
+-- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
+select customer.customer_name, product.product_name , `order`.order_date
+from  customer
+join `order`
+on customer.customer_id = `order`.customer_id
+join order_detail
+on order_detail.order_id = `order`.order_id
+join product
+on product.product_id = order_detail.product_id;
+
+-- Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
+select customer.customer_name, `order`.order_date
+from  customer
+left join `order`
+on customer.customer_id = `order`.customer_id
+where `order`.customer_id is  null;
+
+ -- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn 
+-- (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn. Giá bán của từng loại được tính = odQTY*pPrice)
+select `order`.order_id , `order`.order_date ,sum(order_quality * product_price) as 'doanh_thu'
+from `order`
+join order_detail
+on `order`.order_id = order_detail.order_id
+left join product
+on order_detail.product_id = product.product_id
+group by `order`.order_id;
---
+++ ss3/exercise/query_database_student.sql
@@ -0,0 +1,84 @@
+create database query_database_student;
+use query_database_student;
+
+create table class (
+class_id int primary key auto_increment,
+class_name varchar(60) not null,
+start_date datetime not null,
+`status` bit
+);
+create table student(
+student_id int primary key auto_increment,
+student_name varchar(30) not null,
+address varchar(50),
+phone varchar(20),
+`status` bit,
+class_id int not null,
+foreign key (class_id) references class(class_id)
+);
+
+create table `subject` (
+subject_id int primary key auto_increment,
+subject_name varchar(30) not null,
+credit tinyint not null default 1, check (credit >= 1),
+status bit default 1
+);
+create table mark(
+mark_id int not null primary key auto_increment,
+subject_id int not null,
+student_id int not null,
+unique(subject_id, student_id),
+mark float default 0 check( mark between 0 and 100),
+exam_times tinyint default 1,
+foreign key (subject_id) references `subject`(subject_id),
+foreign key (student_id) references student(student_id)
+);
+
+insert into class
+values (1, 'A1', '2008-12-20', 1),
+ (2, 'A2', '2008-12-22', 1),
+ (3, 'B3', current_date, 0);
+ 
+ insert into student( student_name, address, phone ,`status`, class_id)
+ values ('Hung', 'Ha Noi', '0912113113', 1, 1),
+        ('Hoa', 'Hai phong', null,1, 1),
+        ('Manh', 'HCM', '0123123123', 0, 2);
+        
+insert into `subject`
+values (1, 'CF', 5, 1),
+       (2, 'C', 6, 1),
+       (3, 'HDJ', 5, 1),
+       (4, 'RDBMS', 10, 1);
+	
+insert into mark(subject_id,student_id, mark,exam_times)
+values (1, 1, 8, 1),
+       (1, 2, 10, 2),
+       (2, 1, 12, 1);
+
+-- hiển thị tên bắt đầu bằng chữ h;
+select student_name
+from student
+where student_name like 'h%';
+-- hHiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
+
+select * 
+from class
+where month(start_date) = 12;
+
+-- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
+ select *
+ from `subject`
+ where `subject`.credit between 3 and 5;
+ -- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
+ 
+update student
+set class_id = 2
+where student_name = 'Hung' and class_id = 1;
+
+-- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
+select student.student_name, `subject`.subject_name, mark.mark
+from student
+join mark on student.student_id = mark.student_id
+join `subject` on `subject`.subject_id = mark.subject_id
+order by mark.mark desc;
+
