09d5f7f0d6dff98c5b0c32839303d866959619c5
thanhvo203 <vohaithanh21@gmail.com>
2023-05-16 17:21:27 +0700

    case study task 2 - 5

--- case_study_module_3_csdl/case_study/case_study_furama.sql
+++ case_study_module_3_csdl/case_study/case_study_furama.sql
@@ -49,9 +49,9 @@ ma_bo_phan int,
 foreign key(ma_bo_phan) references bo_phan(ma_bo_phan)
 );
 create table khach_hang(
-ma_loai_khach int primary key auto_increment,
-ma_khach_hang int ,
-foreign key(ma_khach_hang) references loai_khach(ma_loai_khach),
+ma_khach_hang int primary key auto_increment,
+ma_loai_khach int ,
+foreign key(ma_loai_khach) references loai_khach(ma_loai_khach),
 ho_ten varchar(45) not null,
 ngay_sinh date not null,
 gioi_tinh bit(1) not null,
@@ -98,67 +98,76 @@ so_luong int not null
 
 -- Câu 1:
 insert into vi_tri(ten_vi_tri)
-values ('Accountant'),('Service'),('Expert'),('Supervisor'),('Manager'),('Director');
+values ('Quản Lý'),('Nhân viên');
 
 insert into trinh_do(ten_trinh_do)
-values ('Medium Education'),('High Education'),('University'),('After university');
+values ('Trung Cấp'),('Cao Đẳng'),('Đại Học'),('Sau Đại Học');
 
 insert into bo_phan(ten_bo_phan)
-values ('Sale-Marketing'),('Administration'),('Service'),('Manager');
+values ('Sale-Marketing'),('Hành chính'),('Phục vụ'),('Quản lý');
 
 insert into kieu_thue(ten_kieu_thue)
-values ('Hours'),('Day'),('Month'),('Year');
+values ('Hour'),('Day'),('Month'),('Year');
 
 insert into loai_dich_vu(ten_loai_dich_vu)
 values ('Villa'),('House'),('Room');
 
 insert into loai_khach(ten_loai_khach)
-values ('Member'),('Silver'),('Gold'),('Platinum'),('Diamond');
+values ('Diamond'),('Platinum'),('Gold'),('Silver'),('Member');
 
 insert into dich_vu_di_kem(ten_dich_vu_di_kem,gia,don_vi,trang_thai)
-values ('Masssage','10000','VND','Still'),
-        ('Karaoke','20000','VND','Still'),
-        ('Meal','15000','VND','Over'),
-        ('Drinks','5000','VND','Still'),
-        ('Car rental','4000','VND','Over');
+values ('Karaoke','10000','giờ','tiện nghi, hiện tại'),
+        ('Thuê xe máy','10000','chiếc','hỏng 1 xe'),
+        ('Thuê xe đạp','20000','chiếc','tốt'),
+        ('Buffet buổi sáng','15000','suất','đầy đủ đồ ăn, tráng miệng'),
+        ('Buffet buổi trưa','90000','suất','đầy đủ đồ ăn, tráng miệng'),
+        ('Buffet buổi tối','16000','suất','đầy đủ đồ ăn, tráng miệng');
 
 insert into nhan_vien(ho_ten,ngay_sinh,so_cmnd,luong,so_dien_thoai,email,dia_chi,ma_vi_tri,ma_trinh_do,ma_bo_phan)
-values ('Võ Hải Thành','1999-03-20','100100100','1000','0909000111','thanh21@gmail.com','Đà Nẵng',1,1,1),
-	   ('Nguyễn Tấn Tài','2001-01-10','100100100','1000','0909000111','thanh21@gmail.com','Huế',1,2,1),
-	   ('Khải Hoàng Kiệt','1998-02-20','100100100','1000','0909000111','thanh21@gmail.com','Huế',1,2,1),
-	   ('Hoàng Lê Phan','1995-10-20','100100100','1000','0909000111','thanh21@gmail.com','Huế',1,2,1),
-	   ('Thiên Minh Quốc','1995-04-22','100100100','1000','0909000111','thanh21@gmail.com','Huế',1,2,1);
-insert into khach_hang(ma_khach_hang,ho_ten,ngay_sinh,gioi_tinh,so_cmnd,so_dien_thoai,email,dia_chi)
-values (1,'Tài LGBT','20010202',1,'090911100','0909000111','tai@gmail.com','Huế'),      
- (1,'Hường','2001-02-02',0,'090911100','0909000111','tai@gmail.com','Đà Nẵng'),      
- (2,'Thành','1999-02-03',1,'090911100','0909000111','thanh@gmail.com','Đà Nẵng'),      
- (3,'Nguyên','2004-12-17',0,'090911100','0909000111','nguyen@gmail.com','Đà Nẵng'),      
- (4,'Bảo','1999-02-03',1,'090911100','0909000111','bao@gmail.com','Quảng Trị'),      
- (5,'Tiến','1996-10-12',1,'090911100','0909000111','tien@gmail.com','TP. Hồ Chí Minh');   
+values ('Nguyễn Bình An','1970-11-07','456231786','10000000','0901234121','annguyen@gmail.com','295 Nguyễn Tất Thành, Đà Nẵng',1,3,1),
+	   ('Lê Văn Bình','1997-04-09','654231234','7000000','0934212314','binhlv@gmail.com','22 Yên Bái, Đà Nẵng',1,2,2),
+	   ('Hồ Thị Yến','1995-12-12','999231723','14000000','0412352315','thiyen@gmail.com','K234/11 Điện Biên Phủ, Gia Lai',1,3,2),
+	   ('Võ Công Toản','1980-04-04','123231365','17000000','0374443232','toan0404@gmail.com','77 Hoàng Diệu, Quảng Trị',1,4,4),
+	   ('Nguyễn Bỉnh Phát','1999-12-09','454363232','6000000','0902341231','phatphat@gmail.com','43 Yên Bái, Đà Nẵng',2,1,1),
+	   ('Khúc Nguyễn An Nghi','2000-11-08','964542311','7000000','0978653213','annghi20@gmail.com','294 Nguyễn Tất Thành, Đà Nẵng',2,2,3),
+	   ('Nguyễn Hữu Hà','1993-01-01','534323231','8000000','0941234553','nhh0101@gmail.com','4 Nguyễn Chí Thanh, Huế',2,3,2),
+	   ('Nguyễn Hà Đông','1989-09-03','234414123','9000000','0642123111','donghanguyen@gmail.com','111 Hùng Vương, Hà Nội',2,4,4),
+	   ('Tòng Hoang','1982-09-03','256781231','6000000','0245144444','hoangtong@gmail.com','213 Hàm Nghi, Đà Nẵng',2,4,4),
+	   ('Nguyễn Công Đạo','1994-01-08','755434343','8000000','0988767111','nguyencongdao12@gmail.com','6 Hoà Khánh, Đồng Nai',2,3,2);
+insert into khach_hang(ho_ten,ngay_sinh,gioi_tinh,so_cmnd,so_dien_thoai,email,dia_chi,ma_loai_khach)
+values ('Nguyễn Thị Hào','1970-11-07',0,'643431213','0945423362','thihao07@gmail.com','23 Nguyễn Hoàng, Đà Nẵng',5),      
+ ('Phạm Xuân Diệu','1992-08-08',1,'865342123','0954333333','xuandieu92@gmail.com','K77/22 Thái Phiên, Quảng Trị',3),      
+ ('Trương Đình Nghệ','1990-02-27',1,'488645199','0373213122','nghenhan2702@gmail.com','K323/12 Ông Ích Khiêm, Vinh',1),      
+ ('Dương Văn Quan','1981-07-08',1,'543432111','0490039241','duongquan@gmail.com','K453/12 Lê Lợi, Đà Nẵng',1),      
+ ('Hoàng Trần Nhi Nhi','1995-12-09',0,'795453345','0312345678','nhinhi123@gmail.com','224 Lý Thái Tổ, Gia Lai',4),      
+ ('Tôn Nữ Mộc Châu','2005-12-06',0,'732434215','0988888844','tonnuchau@gmail.com','37 Yên Thế, Đà Nẵng',4), 
+ ('Nguyễn Mỹ Kim','1984-04-08',0,'856453123','0912345698','kimcuong84@gmail.com','K123/45 Lê Lợi, Hồ Chí Minh',1),   
+ ('Nguyễn Thị Hào','1999-04-08',0,'965656433','0763212345','haohao99@gmail.com','55 Nguyễn Văn Linh, Kon Tum',3),   
+ ('Trần Đại Danh','1994-07-01',1,'432341235','0643343433','danhhai99@gmail.com','24 Lý Thường Kiệt, Quảng Ngãi',1),   
+ ('Nguyễn Tâm Đắc','1989-07-01',1,'344343432','0987654321','dactam@gmail.com','22 Ngô Quyền, Đà Nẵng',2);   
+ 
  
  insert into dich_vu(ten_dich_vu,dien_tich,chi_phi_thue,so_nguoi_toi_da,ma_kieu_thue,tieu_chuan_phong,mo_ta_tien_nghi_khac,dien_tich_ho_boi,so_tang)
- values ('Dream',100,1000,15,001,'VIP','Comfortable',15,4),
-  ('Forest',50,2000,15,002,'VIP','Comfortable',10,2),
-  ('Mountain',150,1500,15,003,'VIP','Comfortable',5,10),
-  ('Beach',100,3000,15,004,'VIP','Comfortable',20,0);
+ values ('Villa Beach Front',25000,10000000,10,3,'vip','Có hồ bơi',500,4),
+  ('House Princess 01',14000,5000000,7,2,'vip','Có thêm bếp nướng',null,3),
+  ('Room Twin 01',5000,1000000,2,4,'normal','Có tivi',null,null),
+  ('Villa No Beach Front',22000,9000000,8,3,'normal','Có hồ bơi',300,3),
+  ('House Princess 02',10000,4000000,5,3,'normal','Có thêm bếp nướng',null,2),
+  ('Room Twin 02',3000,900000,2,4,'normal','Có tivi',null,null);
 
-insert into hop_dong(ngay_lam_hop_dong,ngay_ket_thuc,tien_dat_coc)
-values ('20231012','20231015',10000),
- ('20230912','20230915',10000),
- ('20231112','20231115',10000),
- ('20231212','20231215',10000);
+insert into hop_dong(ngay_lam_hop_dong,ngay_ket_thuc,tien_dat_coc,ma_nhan_vien,ma_khach_hang,ma_dich_vu)
+values ('2020-12-08','2020-12-08',0,3,1,3),
+ ('2020-07-14','2020-07-21',200000,7,3,1),
+ ('2021-03-15','2021-03-17',50000,3,4,2),
+ ('2021-01-14','2021-01-18',100000,7,5,5),
+ ('2021-07-14','2021-07-15',0,7,2,6),
+ ('2021-06-01','2021-06-03',0,7,7,6),
+ ('2021-09-02','2021-09-05',100000,7,4,4),
+ ('2021-06-17','2021-06-18',150000,3,4,1),
+ ('2020-11-19','2020-11-19',0,3,4,3),
+ ('2021-04-12','2021-04-14',0,10,3,5),
+ ('2021-04-25','2021-04-25',0,2,2,1),
+ ('2021-05-25','2021-05-27',0,7,10,1);
  
- insert into hop_dong_chi_tiet(so_luong)
- values
-  (10),
-  (10),
-  (10);
--- Cấu 2: 
--- select * from nhan_vien where ho_ten like 'H%' or ho_ten like 'K%' or ho_ten like 'T%' and length(ho_ten) <= 15;
--- Câu 3: 	
--- select * from khach_hang where ngay_sinh between 18 and 50 or dia_chi = 'Đà Nẵng' or dia_chi ='Quãng Trị';
--- Câu 4: 
--- select * from 
--- Câu 5: 
-
-
+ insert into hop_dong_chi_tiet(so_luong,ma_hop_dong, ma_dich_vu_di_kem)
+ values  (5,2,4),(8,2,5),(15,2,6),(1,3,1),(11,3,2),(1,1,3),(2,1,2),(2,12,2);
